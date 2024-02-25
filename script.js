6173f087d75b9eb39a2a93156aebd99d0344dcdf
xikmatilla6229 <xikmatilla996@gmail.com>
2024-02-25 10:47:49 +0500

    java2

---
+++ script.js
@@ -0,0 +1,12 @@
+let a=prompt('string typidagi malumotni kiriting')
+let b=+prompt('Number typedagi malumotni kiriting')
+let c=( Boolean( prompt( "Boolean typedagi malumot kiriting")))
+let d=confirm('hamma malumotlarni tog`ri kiritganingnizga ichonchingiz komilmi')
+
+
+alert(`
+siz kiritgan ${a} sozi ${typeof (a)}ga tegishli
+siz kiritgan ${b} raqami ${typeof (b)}ga tegishli
+siz kiritgan ${c} sozi ${typeof(c)}ga tegishli
+`)
+console.log(a.toUpperCase())
